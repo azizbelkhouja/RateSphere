bb1e46f0dfb7d838b831d1da7f6f6083fb404314
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-01-02 23:09:30 +0100

    Book

---
+++ backend/ratesphere/src/main/java/com/ratesphere/BookController.java
@@ -0,0 +1,23 @@
+package com.ratesphere;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<List<Book>> allBooks() {
+        return new ResponseEntity<List<Book>>(bookService.allBooks(), HttpStatus.OK);
+    }
+}
