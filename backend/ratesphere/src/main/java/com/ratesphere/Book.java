47e71c4bdc2b2214ee2fe002be61ad7fabb7d058
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-01-03 18:52:45 +0100

    replaced the @Data annotation with traditional getters

--- backend/ratesphere/src/main/java/com/ratesphere/Book.java
+++ backend/ratesphere/src/main/java/com/ratesphere/Book.java
@@ -1,8 +1,5 @@
 package com.ratesphere;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,9 +8,6 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import java.util.List;
 
 @Document(collection = "books")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Book {
 
     @Id
@@ -22,10 +16,100 @@ public class Book {
     private String title;
     private String releaseDate;
     private String trailerLink;
-    private List<String> genres;
     private String poster;
     private List<String> backdrops;
+    private List<String> genres;
     @DocumentReference
     private List<Review> reviewIds;
 
+    // No-argument constructor
+    public Book() {
+    }
+
+    // All-argument constructor
+    public Book(ObjectId id, String imdbId, String title, String releaseDate, String trailerLink,
+                String poster, List<String> backdrops, List<String> genres, List<Review> reviewIds) {
+        this.id = id;
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genres = genres;
+        this.reviewIds = reviewIds;
+    }
+
+    // Getters and Setters
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public void setTrailerLink(String trailerLink) {
+        this.trailerLink = trailerLink;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public List<String> getBackdrops() {
+        return backdrops;
+    }
+
+    public void setBackdrops(List<String> backdrops) {
+        this.backdrops = backdrops;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<Review> getReviewIds() {
+        return reviewIds;
+    }
+
+    public void setReviewIds(List<Review> reviewIds) {
+        this.reviewIds = reviewIds;
+    }
 }
