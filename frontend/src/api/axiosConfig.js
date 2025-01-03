f915cf3299e2ff3f0c2a70378ec1f28d6fcecb09
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-01-03 22:57:31 +0100

    axios true

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL:'https://9c96-103-106-239-104.ap.ngrok.io/',
-    headers: {"ngrok-skip-browser-warning"}
+    headers: {"ngrok-skip-browser-warning": "true"}
 })
