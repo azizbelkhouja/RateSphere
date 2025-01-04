e0bc4b690476057df170399695a125283b289ef1
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-01-04 23:30:22 +0100

    header styling done

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav";
@@ -9,10 +9,10 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
  
 return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="light" variant="dark" expand="lg">
         <Container fluid>
-            <Navbar.Brand href="/" style={{"color":'gold'}}>
-                <FontAwesomeIcon icon ={faVideoSlash}/>Gold
+            <Navbar.Brand href="/" style={{color:'gold'}}>
+                <FontAwesomeIcon icon ={faBook}/> RateSphere
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
@@ -21,11 +21,17 @@ return (
                         style={{maxHeight: '100px'}}
                         navbarScroll
                     >
-                    <NavLink className ="nav-link" to="/">Home</NavLink>
-                    <NavLink className ="nav-link" to="/watchList">Watch List</NavLink>      
+                    <NavLink className="nav-link" to="/" style={{ color: 'black', textDecoration: 'none' }}>
+                        Home
+                    </NavLink>
+                    <NavLink className="nav-link" to="/watchList" style={{ color: 'black', textDecoration: 'none' }}>
+                        Watch List
+                    </NavLink>
+      
                 </Nav>
-                <Button variant="outline-info" className="me-2">Login</Button>
-                <Button variant="outline-info">Register</Button>
+                <Button style={{ color: 'white', backgroundColor: 'black', borderColor: 'black' }} className="me-2"> Login </Button>
+
+                <Button style={{ color: 'black', backgroundColor: 'white', borderColor: 'black' }}>Register</Button>
             </Navbar.Collapse>
         </Container>
     </Navbar>
